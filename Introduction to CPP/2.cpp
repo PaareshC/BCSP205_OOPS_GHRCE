cb4a1055a635d6741da8a5f22251f233569ad26d
PaareshC <paareshchaudhari@gmail.com>
2021-02-14 13:20:21 +0530

    Addition of 2 numbers Done

---
+++ Introduction to CPP/2.cpp
@@ -0,0 +1,16 @@
+/* 
+Write a CPP program 
+to add two integers
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	int alpha1,alpha2;
+	cout<<"Enter Number 1 = ";
+	cin>>alpha1;
+	cout<<endl<<"Enter Number 2 = ";
+	cin>>alpha2;
+	cout<<endl<<"Sum of "<<alpha1<<" and "<<alpha2<< " is "<<alpha1+alpha2;
+	return 0;
+}
+
