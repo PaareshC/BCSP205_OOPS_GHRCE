32cf7ab6fb5cd19448df1753b6007b8fb95884ea
PaareshC <paareshchaudhari@gmail.com>
2021-02-14 12:55:05 +0530

    Taking Input Done

---
+++ Introduction to CPP/1.cpp
@@ -0,0 +1,12 @@
+/* 
+Write a Program to input 
+a students age and print it*/
+#include<iostream>
+using namespace std;
+int main(){
+	int age;
+	cout<<"Enter Student age = ";
+	cin>>age;
+	cout<<endl<<"Age of Student is "<<age;
+	return 0;
+}
